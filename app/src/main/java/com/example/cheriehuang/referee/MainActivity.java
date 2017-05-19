7d0d1cd1bce9df264927b4ca501477c64b59e2b2
ch3riee <cheriemhuang@gmail.com>
2017-05-19 13:53:37 -0700

    Third Commit: Working toolbar with overflow menu. Currently button clicks respond with a toast event

--- app/src/main/java/com/example/cheriehuang/referee/MainActivity.java
+++ app/src/main/java/com/example/cheriehuang/referee/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.cheriehuang.referee;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.Menu;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,7 +24,48 @@ public class MainActivity extends AppCompatActivity {
     private void initToolbar() {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
-        setTitle(getString(R.string.app_name));
-        mToolbar.setTitleTextColor(android.graphics.Color.WHITE);
+        //setTitle(getString(R.string.app_name));
+       // mToolbar.setTitleTextColor(android.graphics.Color.WHITE);
+        //these are to enable home back up functionality
+        //getSupportActionBar().setHomeButtonEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        tb.inflateMenu(R.menu.toolbar_menu);
+        tb.setOnMenuItemClickListener(
+                new Toolbar.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        return onOptionsItemSelected(item);
+                    }
+                });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home_button:
+                //just testing our buttons with Toast for now
+                Toast.makeText(this, "Home button selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.notifications:
+                Toast.makeText(this, "Notifications selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.profile:
+                Toast.makeText(this, "Profile selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.settings:
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
