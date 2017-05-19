a0fd6c6bdec72f50519af07c0e856ec6f547cd18
ch3riee <cheriemhuang@gmail.com>
2017-05-18 23:38:53 -0700

    Second commit working menu collapse and scrolling.

--- app/src/main/java/com/example/cheriehuang/referee/MainActivity.java
+++ app/src/main/java/com/example/cheriehuang/referee/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.cheriehuang.referee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+
+
+
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        setTitle(getString(R.string.app_name));
+        mToolbar.setTitleTextColor(android.graphics.Color.WHITE);
     }
 }
